e7839d33f7e29359c85a623e6b08a5bd9eb1ba48
Oleg Tsybulskyi <helg37@gmail.com>
2025-01-25 09:43:39 +0200

    Add log_execution_time function.

--- src/env.py
+++ src/env.py
@@ -1,9 +1,11 @@
 from decouple import config
 from logging_config import logger
 
+
 SOME_SECRET = config("SOME_SECRET", default=None)
 assert SOME_SECRET is not None
 SOME_SECRET = str(SOME_SECRET).strip()
+
 
 if __name__ == "__main__":
     logger.info("Environment variables:")
--- src/logging_config.py
+++ src/logging_config.py
@@ -1,5 +1,5 @@
 import logging
-
+import time
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -7,3 +7,19 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger()
+
+
+# Function for logging execution times with high accuracy
+def log_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time_sec = end_time - start_time  # Execution time in seconds
+        # execution_time_ns = execution_time_sec * 1_000_000_000  # Conversion to nanoseconds
+        logger.info(
+            f"Function '{func.__name__}' was performed for {execution_time_sec:.4f} seconds."
+        )
+        return result
+
+    return wrapper
